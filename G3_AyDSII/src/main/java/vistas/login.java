c2767e69008bac396aff3903589daea263229cda
FranciscoVeronING <franciscoveron.ing@gmail.com>
2025-03-29 15:23:55 -0300

    cambios

--- G3_AyDSII/src/main/java/vistas/login.java
+++ G3_AyDSII/src/main/java/vistas/login.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtf_user;
+	private JTextField txtf_pass;
 
 	/**
 	 * Launch the application.
@@ -37,6 +45,55 @@ public class login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_centro = new JPanel();
+		contentPane.add(panel_centro, BorderLayout.CENTER);
+		panel_centro.setLayout(new GridLayout(3, 2, 0, 0));
+		
+		JPanel panel_6 = new JPanel();
+		panel_centro.add(panel_6);
+		
+		JLabel lblNewLabel = new JLabel("usuario : ");
+		panel_6.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_centro.add(panel_1);
+		
+		txtf_user = new JTextField();
+		panel_1.add(txtf_user);
+		txtf_user.setColumns(10);
+		
+		JPanel panel_4 = new JPanel();
+		panel_centro.add(panel_4);
+		
+		JLabel lblNewLabel_1 = new JLabel("Puerto : ");
+		panel_4.add(lblNewLabel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_centro.add(panel_2);
+		
+		txtf_pass = new JTextField();
+		panel_2.add(txtf_pass);
+		txtf_pass.setColumns(10);
+		
+		JPanel panel_5 = new JPanel();
+		panel_centro.add(panel_5);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		panel_5.add(lblNewLabel_2);
+		
+		JPanel panel_3 = new JPanel();
+		panel_centro.add(panel_3);
+		
+		JButton btn_inicio = new JButton("Iniciar sesión");
+		panel_3.add(btn_inicio);
+		
+		JPanel panel_norte = new JPanel();
+		contentPane.add(panel_norte, BorderLayout.NORTH);
+		
+		JLabel lbl_title = new JLabel("Bienvenido/a!");
+		panel_norte.add(lbl_title);
 	}
 
 }
--- G3_AyDSII/src/main/java/vistas/vista_conversacion.java
+++ G3_AyDSII/src/main/java/vistas/vista_conversacion.java
@@ -5,11 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
 
 public class vista_conversacion extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtf_mensaje;
 
 	/**
 	 * Launch the application.
@@ -37,6 +44,34 @@ public class vista_conversacion extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_norte = new JPanel();
+		contentPane.add(panel_norte, BorderLayout.NORTH);
+		
+		txtf_mensaje = new JTextField();
+		panel_norte.add(txtf_mensaje);
+		txtf_mensaje.setColumns(10);
+		
+		JButton btn_Enviar = new JButton("Enviar");
+		panel_norte.add(btn_Enviar);
+		
+		JPanel panel_central = new JPanel();
+		contentPane.add(panel_central, BorderLayout.CENTER);
+		
+		JList list = new JList();
+		list.setVisibleRowCount(100);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		panel_central.add(list);
+		
+		JPanel panel_sur = new JPanel();
+		contentPane.add(panel_sur, BorderLayout.SOUTH);
+		
+		JButton btn_ver_perfil = new JButton("Ver Perfil");
+		panel_sur.add(btn_ver_perfil);
+		
+		JButton btnVolver = new JButton("Volver");
+		panel_sur.add(btnVolver);
 	}
 
 }
